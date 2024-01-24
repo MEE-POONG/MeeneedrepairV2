e750430328c27ff9f4715789517c278017cc0c8c
l3enzlz <150216191+l3enzlz@users.noreply.github.com>
2024-01-24 15:25:14 +0700

    nav

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,11 @@
+import TheNavbar from "./Navbar"
+
 const TheLayout: React.FC = () => {
     return (
         <>
-        <></>
+        <TheNavbar></TheNavbar>
+        
         </>
     )
 }
+export default TheLayout
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,154 +1,184 @@
-
-import React from "react";
-import {
-    Navbar,
-    MobileNav,
-    Typography,
-    Button,
-    IconButton,
-    
-    Card,
-} from "@material-tailwind/react";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { HiBars3BottomLeft, HiOutlineXMark, HiUser, HiChevronDown, HiChevronUp, HiChevronRight } from "react-icons/hi2";
+import Cookies from 'js-cookie';
 
 
-const TheNavbar: React.FC = () => {
-    const [openNav, setOpenNav] = React.useState(false);
 
-    React.useEffect(() => {
-        window.addEventListener(
-            "resize",
-            () => window.innerWidth >= 960 && setOpenNav(false),
-        );
-    }, []);
+const Navbar: React.FC = () => {
 
-    const navList = (
-        <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal"
-            >
-                <a href="#" className="flex items-center">
-                    Pages
-                </a>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal"
-            >
-                <a href="#" className="flex items-center">
-                    Account
-                </a>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal"
-            >
-                <a href="#" className="flex items-center">
-                    Blocks
-                </a>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal"
-            >
-                <a href="#" className="flex items-center">
-                    Docs
-                </a>
-            </Typography>
-        </ul>
-    );
-    return (
-        <>
-            <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
-                <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
-                    <div className="flex items-center justify-between text-blue-gray-900">
-                        <Typography
-                            as="a"
-                            href="#"
-                            className="mr-4 cursor-pointer py-1.5 font-medium"
-                        >
-                            Material Tailwind
-                        </Typography>
-                        <div className="flex items-center gap-4">
-                            <div className="mr-4 hidden lg:block">{navList}</div>
-                            <div className="flex items-center gap-x-1">
-                                <Button
-                                    variant="text"
-                                    size="sm"
-                                    className="hidden lg:inline-block"
-                                >
-                                    <span>Log In</span>
-                                </Button>
-                                <Button
-                                    variant="gradient"
-                                    size="sm"
-                                    className="hidden lg:inline-block"
-                                >
-                                    <span>Sign in</span>
-                                </Button>
-                            </div>
-                            <IconButton
-                                variant="text"
-                                className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-                                ripple={false}
-                                onClick={() => setOpenNav(!openNav)}
-                            >
-                                {openNav ? (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        className="h-6 w-6"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M6 18L18 6M6 6l12 12"
-                                        />
-                                    </svg>
-                                ) : (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="h-6 w-6"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M4 6h16M4 12h16M4 18h16"
-                                        />
-                                    </svg>
-                                )}
-                            </IconButton>
-                        </div>
-                    </div>
-                    <MobileNav open={openNav}>
-                        {navList}
-                        <div className="flex items-center gap-x-1">
-                            <button className="">
-                                <span>Log In</span>
-                            </button>
-                            <button   className="">
-                                <span>Sign in</span>
-                            </button>
-                        </div>
-                    </MobileNav>
-                </Navbar>
+  const [loggedInUser, setLoggedInUser] = useState<any>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const userDataFromCookies = Cookies.get('user');
+      if (userDataFromCookies) {
+        const parsedUser = JSON.parse(userDataFromCookies);
+        setLoggedInUser(parsedUser);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const Links = [
+    { name: 'หน้าแรก', link: `/` },
+    {
+      name: 'เกี่ยวกับเรา',
+      link: 'about',
+      children: [
+        { name: 'เกี่ยวกับเรา', link: `./../about` },
+        { name: 'ข่าวสาร', link: `./../news` },
+        { name: 'บทความ', link: `./../blog` },
+      ],
+    },
+    { name: 'บริการของเรา', link: `./../services` },
+    { name: 'สินค้า', link: `./../products` },
+    { name: 'ประวัติ', link: `./../products` },
+  ];
+
+  const [scroll, setScroll] = useState(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScroll(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  const [isOpen, setIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const toggleDropdown = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <nav className="font-fontTH01">
+      <div className="" style={{ backgroundColor: scroll > 50 ? "" : "#0f172a" }}>
+        <div className=" container mx-auto flex items-center justify-between md:rounded-[100px] px-36 drop-shadow-2xl" style={{ backgroundColor: scroll > 50 ? "#F4F5F5" : "" }}>
+          {/* <div className=" container  " style={{ backgroundColor: scroll > 50 ? "#F4F5F5" : "" }}> */}
+          <div className="flex items-center  py-5 ">
+            <div onClick={() => setIsOpen(!isOpen)}
+              className="md:hidden duration-700 ease-in-out "
+              style={{ color: scroll > 50 ? "" : "#F4F5F5" }}>
+              {
+                isOpen ? <HiOutlineXMark size={38} /> : <HiBars3BottomLeft size={38} />
+              }
             </div>
-        </>
-    )
+            <div className="" >
+              <Image
+                src="/images/logo.png"
+                width={100}
+                height={100}
+                className="w-[50px]" alt="" />
+            </div>
+          </div>
 
-}
-export default TheNavbar
+          {/* nav link here */}
+          <ul className="md:flex gap-5 hidden mr-96 font-medium text-sm md:text-lg items-center">
+            {Links.map((link) => (
+              <li key={link.name} className="hover:border-b-2  hover:border-natural04"
+                style={{ color: scroll > 50 ? "" : "#F4F5F5" }}>
+                {link.children ? (
+                  <div
+                    className="dropdown"
+                    onClick={toggleDropdown}
+                  >
+                    <button
+                      className="btn btn-link flex items-center"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded={open ? 'true' : 'false'}
+                    >
+                      {link.name}
+
+                      <span
+                        className="">
+                        {
+                          open ? <HiChevronUp size={20} /> : <HiChevronDown size={20} />
+                        }
+                      </span>
+                    </button>
+
+                    <ul
+                      className="dropdown-menu absolute bg-secondary1 p-3 rounded-lg drop-shadow-lg"
+                      aria-labelledby="dropdownMenuButton"
+                      style={{ display: open ? 'block' : 'none' }}
+                    >
+                      {link.children.map((child) => (
+                        <li key={child.name} className="my-4 pl-2 hover:border-l-2 hover:border-natural01"
+                          style={{ color: scroll > 50 ? "#F4F5F5" : "" }}
+                        >
+                          <a href={child.link}>{child.name}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ) : (
+                  <a href={link.link}>{link.name}</a>
+                )}
+              </li>
+            ))}
+          </ul>
+
+
+          {/* login && badket */}
+          <ul className="flex gap-3 font-semibold items-center text-base">
+
+            <li className="hover:border-b-2 hover:border-natural04" style={{ color: scroll > 50 ? "" : "#F4F5F5" }}>
+              {loggedInUser ? (
+                <Link href={`./../profile/${loggedInUser.id}`} className="flex items-center">
+                  <HiUser size={20} />
+                  <span className="hidden lg:block">{loggedInUser.email}</span>
+                </Link>
+              ) : (
+                <button className="flex items-center">
+                  <HiUser size={20} />
+                  <a href="./login" className="hidden lg:block text-sm md:text-lg ">เข้าสู่ระบบ</a>
+                </button>
+              )}
+            </li>
+            <div className="bg-natural04 w-[1px] h-10 "></div>
+
+            <li className="mr-5 ">
+              <Link href="./shoppingCart">
+                <svg xmlns="http://www.w3.org/500/svg" width="20" height="20" viewBox="0 0 30 30" fill="none">
+                  <path d="M8.5 18H6.5V26H8.5V18Z" fill="url(#paint0_linear_220_322)" />
+                  <path d="M13.5 18H11.5V26H13.5V18Z" fill="url(#paint1_linear_220_322)" />
+                  <path d="M18.5 18H16.5V26H18.5V18Z" fill="url(#paint2_linear_220_322)" />
+                  <path d="M23.5 18H21.5V26H23.5V18Z" fill="url(#paint3_linear_220_322)" />
+                  <path d="M28.6051 9.5L23.6289 0H21.3711L26.3473 9.5H3.65269L8.62887 0H6.37113L1.39494 9.5H0V16.4667L2.6785 30H27.3198L30 16.599V9.5H28.6051ZM28 16.401L25.6802 28H4.3215L2 16.2708V16H28V16.401ZM28 14H2V11.5H28V14Z" fill="url(#paint4_linear_220_322)" />
+                  <defs>
+                    <linearGradient id="paint0_linear_220_322" x1="6.5" y1="21.9835" x2="8.3642" y2="21.9835" gradientUnits="userSpaceOnUse">
+                      <stop stopColor="#CA0808" />
+                      <stop offset="1" stopColor="#0FC0E7" />
+                    </linearGradient>
+                    <linearGradient id="paint1_linear_220_322" x1="11.5" y1="21.9835" x2="13.3642" y2="21.9835" gradientUnits="userSpaceOnUse">
+                      <stop stopColor="#CA0808" />
+                      <stop offset="1" stopColor="#0FC0E7" />
+                    </linearGradient>
+                    <linearGradient id="paint2_linear_220_322" x1="16.5" y1="21.9835" x2="18.3642" y2="21.9835" gradientUnits="userSpaceOnUse">
+                      <stop stopColor="#CA0808" />
+                      <stop offset="1" stopColor="#0FC0E7" />
+                    </linearGradient>
+                    <linearGradient id="paint3_linear_220_322" x1="21.5" y1="21.9835" x2="23.3642" y2="21.9835" gradientUnits="userSpaceOnUse">
+                      <stop stopColor="#CA0808" />
+                      <stop offset="1" stopColor="#0FC0E7" />
+                    </linearGradient>
+                    <linearGradient id="paint4_linear_220_322" x1="-2.48931e-07" y1="14.9383" x2="27.963" y2="14.9383" gradientUnits="userSpaceOnUse">
+                      <stop stopColor="#CA0808" />
+                      <stop offset="1" stopColor="#0FC0E7" />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
