ba77a9b90ed9b6a12411cd6150efe1f68108e292
l3enzlz <150216191+l3enzlz@users.noreply.github.com>
2024-02-08 16:33:46 +0700

    cart

---
+++ src/container/products/ShowProduct copy.tsx
@@ -0,0 +1,117 @@
+// ShowproductsCard.tsx
+import React, { useState, useEffect } from "react";
+import { LiaCartArrowDownSolid, LiaHeartSolid } from "react-icons/lia";
+import Link from "next/link";
+import Cookies from 'js-cookie';
+
+interface Products {
+  id: number;
+  productname: string;
+  price: string;
+  description: string;
+  imgFirst: string;
+  quantity?: number;
+}
+
+const ShowproductsCard: React.FC = () => {
+  const [cartItems, setCartItems] = useState<Products[]>([]);
+  const [newsData, setNewsData] = useState<Products[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/products');
+        const data = await response.json();
+        setNewsData(data.products);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error:', error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const addToCart = (productId: number) => {
+    const productToAdd = newsData.find((product) => product.id === productId);
+
+    if (productToAdd) {
+      const updatedCart = [...cartItems, { ...productToAdd, quantity: 1 }];
+      setCartItems(updatedCart);
+      Cookies.set('cartItems', JSON.stringify(updatedCart));
+    }
+  };
+
+  const saveToOrderList = async (productId: number) => {
+    try {
+      const response = await fetch('/api/orderlist', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ productId }),
+      });
+
+      if (response.ok) {
+        console.log('Product ID saved to OrderList successfully');
+      } else {
+        console.error('Failed to save Product ID to OrderList');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          newsData.map((product) => (
+            <div key={product.id} className="bg-white shadow-xl rounded-md overflow-hidden">
+              <div className="flex md:flex-wrap items-center">
+                <div className="w-[230px] md:w-full h-[200px] md:rounded-tr-lg md:rounded-tl-lg overflow-hidden">
+                  <Link href={`/products/${product.id}`}>
+                    <img src={product.imgFirst} alt="" className="w-[200PX] h-[200] object-cover rounded-xl" />
+                  </Link>
+                </div>
+
+                <div className="ml-0 w-full md:mt-2 px-3">
+                  <div className="flex flex-wrap justify-between md:mt-5">
+                    <Link href={`/products/${product.id}`}>
+                      <p className="text-black text-xs">{product.productname}</p>
+                    </Link>
+                  </div>
+                  <p className="text-xs md:text-base font-bold line-clamp-2 text-black hover:text-amber-400 mt-2">
+                    {product.description}
+                  </p>
+
+                  <div className="flex flex-wrap my-3 justify-between md:mt-2">
+                    <p className="text-red-400 text-[16px]" style={{ marginTop: 'auto', textShadow: '1px 2px 2px rgba(0, 0, 0, 0.5)' }}>
+                      {product.price} Bath
+                    </p>
+                    <div className="flex items-center">
+                      <button className="text-red-400 hover:text-red-900" onClick={() => { addToCart(product.id); saveToOrderList(product.id); }}>
+                        <p className="text-orange-600 text-[16px]" style={{ marginTop: 'auto' }}>
+                          <LiaCartArrowDownSolid className="text-[20px] ml-32" />
+                        </p>
+                      </button>
+                    </div>
+                    <p className="text-orange-600 text-[16px]" style={{ marginTop: 'auto' }}>
+                      <LiaHeartSolid className="text-[20px]" />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ShowproductsCard;
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -6,148 +6,134 @@ import Link from "next/link";
 import Cookies from 'js-cookie';
 
 interface Product {
-    id: number;
-    productname: string;
-    price: number;
-    imgFirst: string;
-    description: string;
-    quantity: number;
+  id: number;
+  productname: string;
+  price: number;
+  imgFirst: string;
+  description: string;
+  quantity: number;
 }
 
 const CartPage: React.FC = () => {
-    const [cartItems, setCartItems] = useState<Product[]>([]);
+  const [cartItems, setCartItems] = useState<Product[]>([]);
 
-    useEffect(() => {
-        const getCartItemsFromCookies = () => {
-            const cartItemsFromCookies = Cookies.get('cart');
-            if (cartItemsFromCookies) {
-                const parsedCartItems = JSON.parse(cartItemsFromCookies);
-                setCartItems(parsedCartItems);
-            }
-        };
+  const updateCart = (updatedCart: Product[]) => {
+    setCartItems(updatedCart);
+    Cookies.set('cart', JSON.stringify(updatedCart));
+  };
 
-        getCartItemsFromCookies();
-    }, []);
+  const calculateTotal = () => {
+    return cartItems.reduce((total, item) => total + (item.price * item.quantity), 0).toFixed(2);
+  };
 
-    const removeFromCart = (productId: number) => {
-        const updatedCart = cartItems.filter(item => item.id !== productId);
-        updateCart(updatedCart);
+
+  const removeFromCart = (productId: number) => {
+    const updatedCart = cartItems.filter(item => item.id !== productId);
+    updateCart(updatedCart);
+  };
+
+  const increaseQuantity = (productId: number) => {
+    const updatedCart = cartItems.map(item => {
+      if (item.id === productId) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
+    updateCart(updatedCart);
+  };
+
+  const decreaseQuantity = (productId: number) => {
+    const updatedCart = cartItems.map(item => {
+      if (item.id === productId && item.quantity > 0) {
+        return { ...item, quantity: item.quantity - 1 };
+      }
+      return item;
+    });
+    updateCart(updatedCart);
+  };
+  useEffect(() => {
+    const getCartItemsFromCookies = () => {
+      const cartItemsFromCookies = Cookies.get('cart');
+      if (cartItemsFromCookies) {
+        const parsedCartItems = JSON.parse(cartItemsFromCookies);
+        setCartItems(parsedCartItems);
+      }
     };
 
-    const increaseQuantity = (productId: number) => {
-        const updatedCart = cartItems.map(item => {
-            if (item.id === productId) {
-                return { ...item, quantity: item.quantity + 1 };
-            }
-            return item;
-        });
-        updateCart(updatedCart);
-    };
+    getCartItemsFromCookies();
+  }, []);
 
-    const decreaseQuantity = (productId: number) => {
-        const updatedCart = cartItems.map(item => {
-            if (item.id === productId && item.quantity > 0) {
-                return { ...item, quantity: item.quantity - 1 };
-            }
-            return item;
-        });
-        updateCart(updatedCart);
-    };
+  const checkout = async () => {
+    try {
+      const response = await fetch('/api/orderlist', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cartItems }),
+      });
 
-    const updateQuantity = (productId: number, newQuantity: number) => {
-        const updatedCart = cartItems.map(item => {
-            if (item.id === productId) {
-                return { ...item, quantity: newQuantity };
-            }
-            return item;
-        });
-        updateCart(updatedCart);
-    };
+      if (response.ok) {
+        console.log('Order placed successfully', cartItems);
+        setCartItems([]);
+        Cookies.remove('cart');
+      } else {
+        console.error('Failed to place order');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+//
 
-    const updateCart = (updatedCart: Product[]) => {
-        setCartItems(updatedCart);
-        Cookies.set('cart', JSON.stringify(updatedCart));
-    };
+  return (
+    <div className="container mx-auto my-24 font-fontTH02 px-3 lg:px-24">
+      <title>ตะกร้าสินค้าของคุณ</title>
 
-    const calculateItemTotal = (price: number, quantity: number) => {
-        return (price * quantity).toFixed(2);
-    };
-
-    const calculateTotal = () => {
-        return cartItems.reduce((total, item) => total + (item.price * item.quantity), 0).toFixed(2);
-    };
-
-    const checkout = async () => {
-        try {
-            const response = await fetch('/api/orderList', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ cartItems }),
-            });
-
-            if (response.ok) {
-                console.log('Order placed successfully');
-                setCartItems([]);
-                Cookies.remove('cart');
-            } else {
-                console.error('Failed to place order');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
-
-
-    return (
-        <div className="container mx-auto my-24 font-fontTH02 px-3 lg:px-24">
-            <title>ตะกร้าสินค้าของคุณ</title>
-
-            <h3 className="text-lg md:text-2xl text-black">รายการสินค้า <span className="">({cartItems.length})</span></h3>
-            <div className="grid grid-flow-row lg:grid-cols-12 gap-2 lg:gap-8 mt-3 lg:mt-8 ">
-                <div className="lg:col-span-9 bg-secondary1 rounded-md">
-                    {cartItems.map((product) => (
-                        <div key={product.id} className="border rounded-lg overflow-hidden shadow-lg p-4 mb-3 bg-white relative">
-                            <img src={`https://addin.co.th/wp-content/uploads/2022/10/desktop-pc-lenovo-thinkcentre-neo-30a-cover.jpg`} alt="" className="w-[200PX] h-[200] object-cover rounded-xl" />
-                            <div className="flex flex-col justify-end absolute top-4 ml-56 mt-5 space-y-2">
-                                <span className="text-sm font-bold text-black">{product.productname}</span>
-                                <span className="text-sm text-black">{product.description}</span>
-                                <div className="flex items-center justify-between mt-2">
-                                    <div className="flex items-center space-x-2">
-                                        <span className="text-sm font-bold text-red-600 hover:text-red-300">฿ {product.price}</span>
-                                    </div>
-                                    <div className="flex justify-between">
-                                        <button onClick={() => decreaseQuantity(product.id)}>-</button>
-                                        <span className="mx-2">{product.quantity}</span>
-                                        <button onClick={() => increaseQuantity(product.id)}>+</button>
-                                        <button onClick={() => removeFromCart(product.id)}>
-                                            <RiDeleteBin6Line className="text-red-500" />
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-
-                <div className="lg:col-span-3 md:h-48 bg-secondary2 rounded-md p-2 lg:p-5 text-center">
-                    <div className="text-left text-xs md:text-sm">
-                        <p className="flex justify-between mb-2">ยอดรวม <strong>฿ {calculateTotal()}</strong></p>
-                        <p className="flex justify-between">ส่วนลด <strong className="text-natural03">฿ - 0.00</strong></p>
-                        <div className="w-full h-0.5 bg-secondary1 mt-5 mb-2"></div>
-                        <p className="flex justify-between"> <strong>ยอดรวมสุทธิ </strong><strong>฿ {calculateTotal()}</strong></p>
-                    </div>
-                    <button
-                        onClick={checkout}
-                        className="mt-5 text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
-                    >
-                        ดำเนินการสั่งซื้อ
+      <h3 className="text-lg md:text-2xl text-black">รายการสินค้า <span className="">({cartItems.length})</span></h3>
+      <div className="grid grid-flow-row lg:grid-cols-12 gap-2 lg:gap-8 mt-3 lg:mt-8 ">
+        <div className="lg:col-span-9 bg-secondary1 rounded-md">
+          {cartItems.map((product) => (
+            <div key={product.id} className="border rounded-lg overflow-hidden shadow-lg p-4 mb-3 bg-white relative">
+              <img src={`https://addin.co.th/wp-content/uploads/2022/10/desktop-pc-lenovo-thinkcentre-neo-30a-cover.jpg`} alt="" className="w-[200PX] h-[200] object-cover rounded-xl" />
+              <div className="flex flex-col justify-end absolute top-4 ml-56 mt-5 space-y-2">
+                <span className="text-sm font-bold text-black">{product.productname}</span>
+                <span className="text-sm text-black">{product.description}</span>
+                <div className="flex items-center justify-between mt-2">
+                  <div className="flex items-center space-x-2">
+                    <span className="text-sm font-bold text-red-600 hover:text-red-300">฿ {product.price}</span>
+                  </div>
+                  <div className="flex justify-between">
+                    <button onClick={() => decreaseQuantity(product.id)}>-</button>
+                    <span className="mx-2">{product.quantity}</span>
+                    <button onClick={() => increaseQuantity(product.id)}>+</button>
+                    <button onClick={() => removeFromCart(product.id)}>
+                      <RiDeleteBin6Line className="text-red-500" />
                     </button>
+                  </div>
                 </div>
+              </div>
             </div>
+          ))}
         </div>
-    );
+
+        <div className="lg:col-span-3 md:h-48 bg-secondary2 rounded-md p-2 lg:p-5 text-center">
+          <div className="text-left text-xs md:text-sm">
+            <p className="flex justify-between mb-2">ยอดรวม <strong>฿ {calculateTotal()}</strong></p>
+            <p className="flex justify-between">ส่วนลด <strong className="text-natural03">฿ - 0.00</strong></p>
+            <div className="w-full h-0.5 bg-secondary1 mt-5 mb-2"></div>
+            <p className="flex justify-between"> <strong>ยอดรวมสุทธิ </strong><strong>฿ {calculateTotal()}</strong></p>
+          </div>
+          <button
+            onClick={checkout}
+            className="mt-5 text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
+          >
+            ดำเนินการสั่งซื้อ
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartPage;
