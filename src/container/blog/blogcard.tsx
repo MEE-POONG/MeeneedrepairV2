49afa0dd3bd4e8b2b95f965c82779a265c0b0476
l3enzlz <150216191+l3enzlz@users.noreply.github.com>
2024-02-13 10:42:52 +0700

    1

--- src/container/blog/blogcard.tsx
+++ src/container/blog/blogcard.tsx
@@ -56,7 +56,7 @@ export default function BlogCard() {
                                 <div className="flex md:flex-wrap items-center">
                                     <div className="w-[350px] md:w-full h-[100px] md:h-[220px]  md:rounded-tr-lg md:rounded-tl-lg overflow-hidden ">
 
-                                        <img className="w-full h-full object-cover" width={100} height={100}
+                                        <Image className="w-full h-full object-cover" width={100} height={100}
                                             src={`https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/${blog.img ? blog.img : 'f701ce08-7ebe-4af2-c4ec-2b3967392900'}/public`}
                                             alt=""
                                         />
